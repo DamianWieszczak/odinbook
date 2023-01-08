60c797a6381ee5c49a33ee91a687ddc3ac7b0b82
DamianWieszczak <damian.wieszczak@gmail.com>
2023-01-08 17:06:29 +0100

    Add friend requests resources, change post view, add index.html.erb for users, add gems for tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :friends, through: :frienships
   has_many :posts, foreign_key: :author_id
-  has_many :friend_requests
+  has_many :friend_requests, foreign_key: :request_receiver_id
   has_many :request_receivers, through: :friend_request
 
   validates :email, uniqueness: true
